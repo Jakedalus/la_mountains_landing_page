312633dd67cd9f77d8035a847fa16dd0a998380a
Jakedalus <jacquedalus@gmail.com>
2021-05-04 18:15:30 -0400

    Add images, style Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,40 @@
 import React from 'react';
+import styled from 'styled-components';
+import logo from '../images/logo.png';
+
+const StyledHeader = styled.header`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	width: 60%;
+	margin: 0 auto;
+`;
+
+const StyledNav = styled.nav`
+	ul {
+		li {
+			list-style: none;
+			display: inline;
+			margin-right: 40px;
+
+			font-size: 10px;
+			font-style: italic;
+			border-bottom: 1px solid black;
+		}
+	}
+`;
 
 const Header = () => {
 	return (
-		<header>
-			<nav>
+		<StyledHeader>
+			<img src={logo} alt='logo' />
+			<StyledNav>
 				<ul>
 					<li>01. HISTORY</li>
 					<li>02. TEAM</li>
 				</ul>
-			</nav>
-		</header>
+			</StyledNav>
+		</StyledHeader>
 	);
 };
 
